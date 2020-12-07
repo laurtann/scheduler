c8fc725a8d86f3f2e112e2e3b77d1de9c7e5dbf7
Laura Tannahill <lntannahill@gmail.com>
2020-12-07 19:31:50 -0400

    Include formatSpots function to make output of spots readable

--- src/components/__tests__/DayListItem.test.js
+++ src/components/__tests__/DayListItem.test.js
@@ -1,20 +1,3 @@
-
-×
-Congrats on completing activity 'Styling the DayListItem'!
-Testing the DayListItem
-Interview Scheduler Assignment
-30 minutes
- Status
-Incomplete
-We've created our DayListItems and added the appropriate styles to them based on a set of props. However, we just noticed something is off. Right now the spots remaining are only showing up as a number and that doesn't match our design.
-
-We are going to use a Test Driven Development approach to update how the spots feature displays the remaining spots available.
-
-Tests
-We can use unit tests to ensure that the messaging is correct for our spots feature.
-
-Create a DayListItem.test.js file in the src/components/__tests__/ folder. Copy and paste the tests into the file.
-
 import React from "react";
 
 import { render, cleanup } from "@testing-library/react";
