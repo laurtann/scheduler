591bb7b9adff61cb44c4b1290a1b17755860c0af
Laura Tannahill <lntannahill@gmail.com>
2020-12-08 13:54:11 -0400

    Create Appointment folder and styles

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './styles.scss';
+// import classNames from 'classnames';
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment"></article>
+  );
+}
