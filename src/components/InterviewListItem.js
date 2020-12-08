71815b71578fbffaa9a9873fb26982c161d6e50c
Laura Tannahill <lntannahill@gmail.com>
2020-12-08 09:39:15 -0400

    Create InterviewListItem file & scss

---
+++ src/components/InterviewListItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './InterviewListItem.scss';
+
+export default function InterviewListItem(props) {
+  return (
+    <li className="interviewers__item">
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      Sylvia Palmer
+    </li>
+  );
+}
