e70e77a4db30bd2680f2e2752226dc3116cbe895
Laura Tannahill <lntannahill@gmail.com>
2020-12-08 19:53:31 -0400

    Add header, show, empty components with conditionals

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,11 @@ import Empty from './Empty';
 
 export default function Appointment(props) {
   return (
-    <article className="appointment">{props.time}</article>
+    <article className="appointment">
+      <Header 
+        time={props.time}
+      />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+    </article>
   );
 }
