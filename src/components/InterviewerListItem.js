8e152b2591e3fe4f3820b9745cefcc23a8c56883
Laura Tannahill <lntannahill@gmail.com>
2020-12-08 12:38:27 -0400

    Refactor Interviewer components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,19 +7,20 @@ export default function InterviewerListItem(props) {
     'interviewers__item--selected': props.selected
   });
 
-  const interviewListItemClassImg = classNames('interviewers__item-image', {
-    'interviewers__item--selected-image': props.selected
-  });
+  // const interviewListItemClassImg = classNames('interviewers__item-image', {
+  //   'interviewers__item--selected-image': props.selected
+  // });
 
 
   return (
-    <li onClick={() => props.setInterviewer(props.name)} className={interviewListItemClass}>
+    // <li onClick={() => props.setInterviewer(props.name)} className={interviewListItemClass}>
+    <li onClick={props.setInterviewer} className={interviewListItemClass}>
       <img
-        className={interviewListItemClassImg}
+        className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      { props.selected ? props.name : "" }
+      {props.selected && props.name}
     </li>
   );
 }
