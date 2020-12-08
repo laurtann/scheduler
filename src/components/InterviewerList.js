29419364c66b646428c74a07a4eaaffa43ef695f
Laura Tannahill <lntannahill@gmail.com>
2020-12-08 10:43:29 -0400

    Add InterviewList function and scss

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './InterviewerList.scss';
+import classNames from 'classnames';
+
+export default function InterviewerList(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  );
+}
+
