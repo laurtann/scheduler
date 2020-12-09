962fb6eae91eafb97fd21e9f56566cf67bc6b5a5
Laura Tannahill <lntannahill@gmail.com>
2020-12-09 16:22:37 -0400

    Remove commented out code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,3 @@
-
-//find obj in state.days arr whose name matches "DAY",
-// with that, access DAY appt arr
-// iterate, compare where id matches id of state.appts
-// return that value
-// if no appts on the day, return []
-
 function getAppointmentsForDay (state, day) {
   const selectedDay = state.days.filter(d => d.name === day);
   if (!state.days.length || !selectedDay.length) {
