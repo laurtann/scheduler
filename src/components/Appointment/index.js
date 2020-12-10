9f238ca1e2f03999e93403b08a043140d39fe1b6
Laura Tannahill <lntannahill@gmail.com>
2020-12-10 19:23:33 -0400

    Add create form component and logic using custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,39 @@ import './styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+import useVisualMode from '../../../src/hooks/useVisualMode';
 // import classNames from 'classnames';
 
+const interviewers = [];
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const BACK = "BACK";
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
-      <Header 
+      <Header
         time={props.time}
       />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={interviewers}
+          // onSave={props.onSave}
+          onCancel={() => transition(BACK)}
+        />
+      )}
+      {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />} */}
     </article>
   );
 }
