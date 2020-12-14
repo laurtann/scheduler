802305708eba3f976e92f2e80d1171b2df6bed97
Laura Tannahill <lntannahill@gmail.com>
2020-12-14 17:33:24 -0400

    Add additional Jest tests to form component

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -40,17 +40,26 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
-  it("calls onSave function when the name is defined", () => {
+  it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { queryByText, getByText, getByPlaceholderText } = render(
-      <Form interviewers={interviewers} onSave={onSave}/>
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
     );
 
-    const input = getByPlaceholderText("Enter Student Name");
-    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } } );
     fireEvent.click(getByText("Save"));
+
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByText("Save"));
+
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null, null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
 });
