8a93be9e2a91edc9989983c286370ff223aefbbd
Laura Tannahill <lntannahill@gmail.com>
2020-12-14 13:50:36 -0400

    Set up Jest test in Appointment and propTypes in DayListItem

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Application from "components/Application";
+
+import Appointment from "components/appointment/index"
+
+it("renders without crashing", () => {
+  render(<Application />);
+});
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+});
